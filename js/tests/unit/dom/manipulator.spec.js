8e54863aa64560225ff68c95809681a00ddda333
GeoSot <geo.sotis@gmail.com>
2021-09-29 02:34:34 +0300

    Manipulator: Add json parse support

--- js/tests/unit/dom/manipulator.spec.js
+++ js/tests/unit/dom/manipulator.spec.js
@@ -1,7 +1,7 @@
 import Manipulator from '../../../src/dom/manipulator'
 
 /** Test helpers */
-import { getFixture, clearFixture } from '../../helpers/fixture'
+import { clearFixture, getFixture } from '../../helpers/fixture'
 
 describe('Manipulator', () => {
   let fixtureEl
@@ -94,7 +94,7 @@ describe('Manipulator', () => {
     })
 
     it('should normalize data', () => {
-      fixtureEl.innerHTML = '<div data-bs-test="false" ></div>'
+      fixtureEl.innerHTML = '<div data-bs-test="false" data-bs-test2=\'{"delay":{"show":100,"hide":10}}\'></div>'
 
       const div = fixtureEl.querySelector('div')
 
@@ -103,8 +103,17 @@ describe('Manipulator', () => {
       div.setAttribute('data-bs-test', 'true')
       expect(Manipulator.getDataAttribute(div, 'test')).toEqual(true)
 
-      div.setAttribute('data-bs-test', '1')
-      expect(Manipulator.getDataAttribute(div, 'test')).toEqual(1)
+      let objectData = { 'Super Hero': ['Iron Man', 'Super Man'], url: 'http://localhost:8080/test?foo=bar' }
+      let dataStr = encodeURIComponent(JSON.stringify(objectData))
+      div.setAttribute('data-bs-test', dataStr)
+      expect(Manipulator.getDataAttribute(div, 'test')).toEqual(objectData)
+
+      objectData = { 'Super Hero': ['Iron Man', 'Super Man'], url: 'http://localhost:8080/test?foo=bar' }
+      dataStr = JSON.stringify(objectData)
+      div.setAttribute('data-bs-test', dataStr)
+      expect(Manipulator.getDataAttribute(div, 'test')).toEqual(objectData)
+
+      expect(Manipulator.getDataAttribute(div, 'test2')).toEqual({ delay: { show: 100, hide: 10 } })
     })
   })
 
